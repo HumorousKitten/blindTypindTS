edad5ced10d83ce06defd9ffbe8bc9381cff3f4d
vladiks <vladvannikov@gmail.com>
2025-03-18 20:53:47 +0400

    add running cat animation in registerPage

--- src/pages/Registration/RegistrationPage.tsx
+++ src/pages/Registration/RegistrationPage.tsx
@@ -2,20 +2,17 @@ import React from 'react'
 import { Form as RegisterForm } from '../../components/form/Form'
 import cl from './_register.module.scss'
 import { server } from '../../server/server'
-
+import { RunningCat } from '../../UI/runningCat/RunningCat'
 
 export const RegisterPage = () => {
-	// async function registerUser(email: string, password: string ,login: string) {
-	// 	const data = await server.registration(login, password, email)
-	// 	console.log(data)
-	// 	setResponse(data)
-	// }
-
 
 	return (
 		<div className={cl.register}>
-			<h2>Регистрация</h2>
-			<RegisterForm />			
+			<div className={cl.registerContent}>
+				<RunningCat classNames={{position: 'absolute'}} widthPx={427} coordPosition={{right: -130, bottom: -50}}/>
+				<h2>Регистрация</h2>
+				<RegisterForm />			
+			</div>
 		</div>
 	)
 }
