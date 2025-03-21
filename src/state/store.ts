59deccf67b9dc10ba992205a6a1b87bf6209adce
vladiks <vladvannikov@gmail.com>
2025-03-21 20:15:18 +0400

    add logic in store for saving letters from simulatorInput

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,3 +1,20 @@
 import { create } from 'zustand';
 
-const useStore = create((set) => ({}))
+interface IStore {
+	requiredLetter: string
+	wrongLetter: string
+	rightLetter: string
+	updateRequiredLetter: (letter: string) => void
+	updateWrongLetter: (letter: string) => void
+	updateRightLetter: (letter: string) => void
+}
+
+const useStore = create<IStore>((set) => ({
+	requiredLetter: '',
+	wrongLetter: '',
+	rightLetter: '',
+
+	updateRequiredLetter: (letter: string) => set({requiredLetter: letter}),
+	updateWrongLetter: (letter: string) => set({wrongLetter: letter}),
+	updateRightLetter: (letter: string) => set({rightLetter: letter}) 
+}))
