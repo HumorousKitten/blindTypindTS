b0393fd95cd3c71fbde880efb9974c0f8fe666f7
vladiks <vladvannikov@gmail.com>
2025-03-24 12:51:49 +0400

    add finalResults after lvl

--- src/components/SimulatorBlock/SimulatorBlock.tsx
+++ src/components/SimulatorBlock/SimulatorBlock.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import { KeyBoardBlock } from '../KeyboardBlock/KeyboardBlock';
-import { SimulatorStr } from '../SimulatorStr/SimulatorStr';
-import { ProgressBar } from '../progressBar/ProgressBar';
-import { Timer } from '../timer/Timer';
-import cl from "./_SimulatorBlock.module.scss";
+import React from 'react'
+import { KeyBoardBlock } from '../KeyboardBlock/KeyboardBlock'
+import { SimulatorStr } from '../SimulatorStr/SimulatorStr'
+import { FinalResults } from '../finalResults/FinalResults'
+import { ProgressBar } from '../progressBar/ProgressBar'
+import { Timer } from '../timer/Timer'
+import cl from './_SimulatorBlock.module.scss'
 
 
 export const SimulatorBlock = () => {
-  return (
-    <div className={cl.MainContainer}>
-      <Timer />
-      <SimulatorStr />
-      <ProgressBar />
-      <KeyBoardBlock />
-    </div>
-  );
-};
- 
+  const [isEndLevel, setIsEndLevel] = React.useState<boolean>(false) 
+
+	return (
+		<div className={cl.MainContainer}>
+			{isEndLevel ? (
+				<FinalResults />
+			) : (
+				<>
+					<Timer />
+					<SimulatorStr setIsEnd = {setIsEndLevel}/>
+				</>
+			)}
+			<ProgressBar mc={isEndLevel ? 'mc' : undefined} />
+			<KeyBoardBlock />
+		</div>
+	)
+}
--- src/components/progressBar/ProgressBar.tsx
+++ src/components/progressBar/ProgressBar.tsx
@@ -3,11 +3,15 @@ import { useStore } from '../../state/store';
 
 import cl from './_progressBar.module.scss';
 
-export const ProgressBar = () => {
+interface IPropgressBarProps {
+	mc?: 'mc' 
+}
+
+export const ProgressBar = ({mc}) => {
 	const progressBarWidth = useStore(state => state.progressBarWidth)
 
 	return (
-		<div className={cl.mainBar}>
+		<div className={`${cl.mainBar} ${mc ? cl.mc : cl.ml}`}>
 			<div className={cl.secondBar}></div>
 			<div className={cl.thirdBar} style = {{width: `${progressBarWidth.toFixed(3)}%`}}></div>
 		</div>
