8ff9f231eac0ab8ec5d9697f0a4372a5c7ce91bf
vladiks <vladvannikov@gmail.com>
2025-03-26 20:52:01 +0400

    add button for navigatin to levelsPage

--- src/components/SimulatorBlock/SimulatorBlock.tsx
+++ src/components/SimulatorBlock/SimulatorBlock.tsx
@@ -4,6 +4,7 @@ import { SimulatorStr } from '../SimulatorStr/SimulatorStr'
 import { FinalResults } from '../finalResults/FinalResults'
 import { ProgressBar } from '../progressBar/ProgressBar'
 import { Timer } from '../timer/Timer'
+import { NavigateToLevels } from '../navigateToLevels/NavigateToLevels'
 import cl from './_SimulatorBlock.module.scss'
 
 
@@ -17,10 +18,12 @@ export const SimulatorBlock = () => {
 			) : (
 				<>
 					<Timer />
+					<NavigateToLevels />
 					<SimulatorStr setIsEnd = {setIsEndLevel}/>
 				</>
 			)}
 			<ProgressBar mc={isEndLevel ? 'mc' : undefined} />
+			{isEndLevel ? <NavigateToLevels mc='mc'/> : null}
 			<KeyBoardBlock />
 		</div>
 	)
---
+++ src/components/navigateToLevels/NavigateToLevels.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../UI/button/Button';
+
+import cl from './_navigateToLevels.module.scss'
+
+import bookLevel from '../../assets/img/icons/level_book.svg'
+
+interface INavigateToLevelsProps{
+	mc?: 'mc'
+}
+
+export const NavigateToLevels: FC<INavigateToLevelsProps> = ({mc}) => {
+
+	return (
+		<Button additionalClasses={{display: 'block', background: 'transparent', margin: mc ? 'auto' : undefined}} type={'button'}>
+			<Link className={cl.navigateToLevels} to={'/levels'}>
+				<img src={bookLevel} alt="levelPageIcon" />
+				<span className={cl.level}>Уровень</span>
+				<span className={cl.level}>2.1</span>
+			</Link>
+		</Button>
+	);
+}
+ 
