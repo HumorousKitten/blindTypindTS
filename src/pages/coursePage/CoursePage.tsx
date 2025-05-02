c8147d4d6540870066b7ddfe188f40cc3a3c7f2f
vladiks <vladvannikov@gmail.com>
2025-05-02 15:38:01 +0400

    add coursePage with courses

---
+++ src/pages/coursePage/CoursePage.tsx
@@ -0,0 +1,15 @@
+import { Header } from '../../components/Header/Header';
+import { CourseContent } from '../../components/courseContent/CourseContent';
+
+
+export const CoursePage = () => {
+	return (
+		<>
+			<Header />
+			<CourseContent />
+		</>
+	);
+}
+ 
+
+
